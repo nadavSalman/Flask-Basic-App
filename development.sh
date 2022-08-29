bbac8a95094c6e287af485d4ac0e4ae9d8230689
nadavSalman <nadavops@gmail.com>
2022-08-29 22:07:58 +0300

    Test pos request

--- development.sh
+++ development.sh
@@ -1,14 +1,12 @@
 #!/usr/local/bin/bash
 
-# If FLASK_APP is not set, the command will try to import “app” or “wsgi” (as a “.py” file, or package) 
+# If FLASK_APP is not set, the command will try to import “app” or “wsgi” (as a “.py” file, or package)
 # export FLASK_APP=app
-# Alternative : $ python -m flask 
+# Alternative : $ python -m flask
 
 # To enable all development features, set the FLASK_ENV environment variable to development before calling flask run.
 export FLASK_ENV=development
 flask run
-
-
 
 #  make the server publicly available from any ather client on your network.
 # flask run --host=0.0.0.0
--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 # First we imported the Flask class. An instance of this class will be our WSGI application.
-from flask import Flask
+from flask import Flask, url_for, render_template, request
 from markupsafe import escape
-from flask import url_for
-from flask import render_template
+# from flask import url_for
+# from flask import render_template
+# from flask import request
 
 
-#
-#
+# Based on the doc : https://flask.palletsprojects.com/en/2.2.x/quickstart/
+
+
 app = Flask(__name__)
 
 
@@ -38,19 +40,22 @@ def show_post(post_id):
 def profile(username):
     return f'{username}\'s profile'
 
+
 with app.test_request_context():
     print(url_for('profile', username='John Doe'))
 
-#----------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # HTTP Methods
-# You can use the methods argument of the route() decorator to handle different HTTP methods. 
+# You can use the methods argument of the route() decorator to handle different HTTP methods.
 
 '''
 This example keeps all methods for the route within one function,
 which can be useful if each part uses some common data.
 '''
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -59,7 +64,7 @@ def login():
         return 'GET to end point : /login'
 
 
-#You can also separate views for different methods into different functions.
+# You can also separate views for different methods into different functions.
 # Flask provides a shortcut for decorating such routes with get(), post(), etc. for each common HTTP method.
 @app.get('/login2')
 def login_get():
@@ -70,10 +75,11 @@ def login_get():
 curl --location --request POST 'http://127.0.0.1:5000/login2' \
 --header 'Content-Type: application/json'
 '''
+
+
 @app.post('/login2')
 def login_post():
     return 'GET to end point : /login2'
-
 
 
 # Static Files
@@ -81,12 +87,48 @@ with app.app_context(), app.test_request_context():
     url_for('static', filename='main.css')
 
 
-# Rendering Templates 
+# Rendering Templates
 # Flask will look for templates in the templates folder
+'''
+Automatic escaping is enabled, so if name contains HTML it will be escaped automatically.
+'''
+
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Accessing Request Data
+@app.route('/login-1', methods=['POST', 'GET'])
+def login1():
+    error = None
+    if request.method == 'POST':
+        return valid_login(request.form['username'], request.form['password'])
+    return None,404
+
+
+def valid_login(user_name, password):
+    if user_name == "babun" and password == "123":
+        return {
+            "massage": "your loged in ad system admin",
+            "privilages": "write"
+        }, 200
+    else:
+        return {
+            "massage": "Forbidden access ",
+            "privilages": "none"
+        }, 403
+        
+        
+# Test the endpoint /login-1 use :
+    # curl --location --request POST 'http://127.0.0.1:5000/login-1' \
+    # --header 'Content-Type: application/x-www-form-urlencoded' \
+    # --data-urlencode 'username=babun' \
+    # --data-urlencode 'password=123'
 
 
 if __name__ == '__main__':
